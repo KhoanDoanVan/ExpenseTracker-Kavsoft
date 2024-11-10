80298531ee06799f2381b6b3cd74fb0fac3de18b
khoanisdev <doanvankhoan124@gmail.com>
2024-11-10 16:12:51 +0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Đoàn Văn Khoan on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
